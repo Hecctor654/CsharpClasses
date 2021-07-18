3466386f610e5ab5f62ca13fbbd738ec026db4cf
Jose Preciado <josepreciado9549@gmail.com>
2021-07-17 23:37:47 -0500

    Assignment 1

---
+++ assignments/Assignment1/Assignment1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "jose";
+            int myAge = 28;
+            string wN = "Torrie";
+            int wA = 28;
+            string oK = "Nora";
+            int oA = 8;
+            string yK = "Isla";
+            int yA = 8;
+
+
+            Console.WriteLine($"Hello my name is {myName} i am {myAge} years old.");
+            Console.WriteLine($"My wifes name is {wN} and she is also {wA}.");
+            Console.WriteLine($"{oK}, my first born, is {oA}.");
+            Console.WriteLine($"{yK} is the youngest being {yA} months old!");
+
+                
+            Console.ReadLine();
+        }
+    }
+}
